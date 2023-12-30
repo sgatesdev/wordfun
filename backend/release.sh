16a1be8ec3ce4b4c8ded3c06ad75cf02777ca0af
sgatesdev <sam.j.gates@gmail.com>
2023-12-29 20:23:21 -0500

    Adjusted release script

--- backend/release.sh
+++ backend/release.sh
@@ -7,4 +7,9 @@ echo "Saving docker image to ./docker_images/wordfun_$version.tar.gz..."
 docker save wordfun:$version | gzip > ./docker_images/wordfun_$version.tar.gz
 echo "Done!"
 
+
+# cache version number
+touch ./docker_images/version.txt
+echo "Version: $version, Timestamp: $(date)" > ./docker_images/version.txt
+
 # docker rmi $(docker images -q 'wordfun') -f
